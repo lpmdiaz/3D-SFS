4aed7ceb1542df3a60f64119f2203894972b96e6
lpmdiaz <leo-diaz@hotmail.fr>
2017-05-19 15:53:36 +0100

    Update pop3Dclrt.cpp

--- pop3Dclrt.cpp
+++ pop3Dclrt.cpp
@@ -115,17 +115,11 @@ int main (int argc, char *argv[]) {
 
     // CALCULATE THE GLOBAL COMPOSITE LIKELIHOOD
     getline(global_sfs, line); // retrieving the first and only line of the
-    cout << "Global SFS line: "<< line << "\n"; ////////////////////////////////////////////////////////////////////////////////////////
     line_to_vector(line, sfs); // global SFS file and storing it into a vector
-    cout << "Checking SFS value: " << sfs[42] << "\n"; /////////////////////////////////////////////////////////////////////////////////
     zero_freq_remove(sfs); // remove first and last SFS values
-    cout << "Checking SFS values: " << sfs[41] << " " << sfs[42] << " " << sfs[43] << "\n"; ////////////////////////////////////////////
     double globalSNPsnr = 0; for (int i = 0; i < sfs.size(); i++) globalSNPsnr += sfs[i]; // number of SNPs in global SFS
-    cout << "Global SNPs number: " << globalSNPsnr << "\n"; ////////////////////////////////////////////////////////////////////////////
     double help_fact = calculate_help_fact(globalSNPsnr, argv[2]);
-    cout << "Help factor for debugging purposes (should be 99.9385): " << help_fact << "\n"; ///////////////////////////////////////////
     global_CL = calculate_CL(sfs, globalSNPsnr, help_fact);
-    cout << "Global CL = " << global_CL << "\n"; ///////////////////////////////////////////////////////////////////////////////////////
     sfs.clear();
 
     // CALCULATE WINDOWS CL, CALCULATE AND STORE THE CL RATIO TEST RESULT
@@ -139,7 +133,6 @@ int main (int argc, char *argv[]) {
         CLRT = calculate_CLRT(windows_CL, global_CL);
         clrt_output << setprecision(10) << CLRT << " \n";
         sfs.clear();
-        //cout << CLRT << " "; //////////////////////////////////////////// -nan
     }
 
     // CLOSING FILES
